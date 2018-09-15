5b796f108e60038e73e45ee7212d43cecf8aee89
Sergiy Poddubka <poseal@gmail.com>
2018-09-15 22:21:28 +0300

    add SPRING

--- src/main/java/com/poseal/university/dao/PostgreConnectionUtils.java
+++ src/main/java/com/poseal/university/dao/PostgreConnectionUtils.java
@@ -3,13 +3,12 @@ package com.poseal.university.dao;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.poseal.university.exception.DAOException;
 
@@ -17,20 +16,15 @@ public class PostgreConnectionUtils {
 
     private static final Logger log = LogManager.getLogger(PostgreConnectionUtils.class);
 
-    private static final String DATASOURCE_NAME = "jdbc/my_university_db";
-    private static DataSource dataSource;
-
+    private static ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
     public static Connection getConnection() {
         Connection connection = null;
-        log.debug("Creating connection to database");
+        DataSource dataSource = ((DataSource) context.getBean("dataSource"));
+
         try {
-            Context initContext = new InitialContext();
-            Context envContext = (Context) initContext.lookup("java:/comp/env");
-            dataSource = (DataSource) envContext.lookup(DATASOURCE_NAME);
             connection = dataSource.getConnection();
-            log.debug("Connection opened");
-        } catch (NamingException | SQLException e) {
+        } catch (SQLException e) {
             log.error("Error while creating connection to database!", e);
             throw new DAOException("Error while creating connection to database!", e);
         }
